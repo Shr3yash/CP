5872c71ca40498e9b25b18cac766617536adc7a7
Shreyash <60093808+Shr3yash@users.noreply.github.com>
2022-03-24 03:30:13 +0530

    Update bubble.cpp

--- AllSortOfSorts/bubble.cpp
+++ AllSortOfSorts/bubble.cpp
@@ -9,3 +9,25 @@ public:
     return nums;
     }
 };
+
+
+//Optimanzee
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+    bool swapp = false;
+    for(int i=0; i<nums.size(); i++){
+        for(int i=0; i<nums.size()-1; i++)
+            {
+            if(nums[i]>nums[i+1]) 
+                {
+                (swap(nums[i], nums[i+1])) ;
+                swapp =true;           
+                }
+            }
+            if (!swapp) break;      //if there's no swap in any turn then break.
+        }
+    return nums;
+    }
+};
