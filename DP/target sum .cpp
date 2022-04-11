5959fa475d42d7147a493168845179706edcc6ad
Shreyash <60093808+Shr3yash@users.noreply.github.com>
2022-04-11 13:34:21 +0530

    Create target sum .cpp

---
+++ DP/target sum .cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    
+        
+    /*
+        1, 1, 1, 1, 1
+        +  +  +  +  + 
+        +  +  +  +  -
+        +  +  +  -  -
+        +  +  +  -  +
+        +  +  -  -  -
+        +  +  -  +  -
+    */
+    
+    int n;
+    int target;
+    map<pair<int,int>,int> cache;
+    int DFS(vector<int>& nums,int currSum, int i)
+    {
+        auto find = cache.find({i,currSum});
+        
+        if (find != cache.end())
+            return find->second;
+        
+        if (i == n && currSum == target)
+        {
+            return 1;
+        }
+        if(i >= n)
+            return 0;
+
+        return cache[{i,currSum}] = DFS(nums,currSum + nums[i],i + 1) + DFS(nums,currSum - nums[i],i + 1);
+    }
+    
+    int findTargetSumWays(vector<int>& nums, int S) 
+    {
+        n = nums.size();
+        target = S;
+        
+        if(n == 0)
+            return 0;
+        
+        return DFS(nums,0,0);
+    }
+};
